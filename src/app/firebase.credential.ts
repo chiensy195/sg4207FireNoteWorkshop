63cb40d1de9e81d40b6dc45f0849c23d0f221b40
Dang Chien Sy <chiensy195@gmail.com>
2018-04-08 23:11:44 +0800

    add firebase config file

---
+++ src/app/firebase.credential.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD89fXy8jqhZzTKJdeviaFmnQkomlMO1WA",
+    authDomain: "sg4207-c5692.firebaseapp.com",
+    databaseURL: "https://sg4207-c5692.firebaseio.com",
+    projectId: "sg4207-c5692",
+    storageBucket: "sg4207-c5692.appspot.com",
+    messagingSenderId: "133779126478"
+}
